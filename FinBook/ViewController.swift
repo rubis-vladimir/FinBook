d219cb6608eb0c77dace025d24fc79e061c8a653
rubis-vladimir <rubis-vladimir>
2021-07-11 18:36:47 +0300

    Initial Commit

---
+++ FinBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinBook
+//
+//  Created by Владимир Рубис on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
