d202dc28b6f55080129f82e0311739eda24c270a
Nikita Kelevra <nonamerz5@mail.ru>
2021-07-14 22:27:11 +0300

    Add Elements

--- FinBook/MainTabBarController.swift
+++ FinBook/MainTabBarController.swift
@@ -9,10 +9,10 @@ import UIKit
 
 class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
 
-    override func viewDidAppear(_ animated: Bool) {
-        self.delegate = self
-    }
-    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        if viewController is
-    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        self.delegate = self
+//    }
+//    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+//        if viewController is
+//    }
 }
