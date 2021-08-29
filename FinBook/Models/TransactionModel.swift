369ef8f4e29f5cf125ef5663d298ea5990470176
rubis-vladimir <rubis-vladimir>
2021-08-29 21:25:56 +0300

    Change Models

--- FinBook/Models/TransactionModel.swift
+++ FinBook/Models/TransactionModel.swift
@@ -5,23 +5,17 @@
 //  Created by Владимир Рубис on 11.07.2021.
 //
 
-import Foundation
+import UIKit
 
 struct Transaction {
     var cost: Double
-    var label: String?
-    var category: (String, String) = Category.products
+    var label: String
+    var category: Category
     var date: Date = Date()
-    var note: String = ""
+    var note: String
     
 //    чтобы понять проходит трата или доход - если доход то "true", а если трата "false"
     var incomeTransaction = false
-    
-
-//
-//    static func getTransaction() -> Transaction {
-//        Transaction(cost: 100, label: "Масло", category: .car, date: Date)
-//    }
 }
 
 //extension Transaction {
@@ -31,28 +25,5 @@ struct Transaction {
 //}
 
 
-//struct Category {
-//    var label: String
-//    var image: String
-//}
 
-
-
-enum Category: CaseIterable {
-//    typealias RawValue = (Int, Int)
-    static let products = (label: "Продукты", image: "plus")
-//    case none(
-//    case products = "Продукты"
-//    case clothes = "Одежда"
-//    case house = "Дом"
-//    case health = "Здоровье"
-//    case car = "Машина"
-//    case ticket = "Билеты"
-//    case travelling = "Путешествия"
-//    case credits = "Кредиты"
-//    case study = "Oбучение"
-//    case family = "Семья"
-//    case taxes = "Налоги"
-//    case other = "Другое"
-}
 
