b9b648bf11901fd1bef57db482591aa8932e0ce1
Никита <81251222+NikitaKelevra@users.noreply.github.com>
2022-04-15 07:17:45 +0300

    Заполнил AccountVC комментами

--- FinBook/Controllers/AccountViewController.swift
+++ FinBook/Controllers/AccountViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 protocol NewTransactionViewControllerDelegate {
     func saveTransaction(_ newTransaction: Transact)
 }
-
+// MARK: VC для вывода актуальных транзакций
 class AccountViewController: UIViewController {
     
     // MARK: - IBOutlets
@@ -41,17 +41,18 @@ class AccountViewController: UIViewController {
         setupElements()
         setupSearchBar()
         reloadWalletBalance()
-        reloadTransactArrayToFiltered()
+        sortedByDateAllTransactArrays()
     }
     
     // MARK: - Private func
+    /// Установка размера кнопки `_` и стартового цвета фона TableView
     private func setupElements() {
         addButton.customizeButton(cradius: addButton.frame.width / 2)
         transactionTableView.backgroundColor = UIColor.clear
         view.backgroundColor = Palette.background
     }
     
-    // расчет и вывод баланса кошелька по транзакциям
+    /// Расчет и вывод баланса кошелька по текущим транзакциям
     private func reloadWalletBalance() {
         //        self.transactions = transactions.sorted{ $0.date! < $1.date! }
         var currentBalance = 0.00
@@ -62,19 +63,18 @@ class AccountViewController: UIViewController {
         }
         balanceLabel.text = "\(currentBalance) "
     }
-    
-    private func reloadTransactArrayToFiltered() {
+    /// Сортировка основного массива `transactions` и отфильтрованого `filteredTransactions` по дате
+    private func sortedByDateAllTransactArrays() {
         self.transactions = sortedByDateTransactArray(array: transactions)
         self.filteredTransactions = sortedByDateTransactArray(array: filteredTransactions)
     }
-    
+    /// Перезагрузка/обновление транзакций отображающихся в TableView параллельно в основном потоке
     private func reloadDataTableView() {
         DispatchQueue.main.async {
-            self.transactionTableView.reloadData()  // обновление  списка в основном потоке
+            self.transactionTableView.reloadData()
         }
     }
-    
-    // Setup the search controller
+    /// Стартовые настройки `search controller`
     private func setupSearchBar() {
         searchController.searchBar.delegate = self
         searchController.obscuresBackgroundDuringPresentation = false
@@ -82,12 +82,11 @@ class AccountViewController: UIViewController {
         navigationItem.searchController = searchController
         definesPresentationContext = true
     }
-    
+    /// Функция возврата списка транзакций в зависимости отфильтрованы транзакции в `search controller` или нет
     private func transactionToDisplayAt(indexPath: IndexPath) -> Transact {
         return isFiltering ? filteredTransactions[indexPath.row] : transactions[indexPath.row]
     }
-    
-    //функция сортировки массивов по дате
+    /// Функция сортировки массива `[Transact]` по дате
     private func sortedByDateTransactArray(array: [Transact]) -> [Transact] {
         let transactArray = array.sorted { numLeft, numRight in
             guard let left = numLeft.date else { return true }
@@ -95,46 +94,46 @@ class AccountViewController: UIViewController {
             return left > right
         }
         return transactArray
-    }
-    
-    // MARK: - Navigation - переход и передача данных на TransactionViewController
+    } 
+    // MARK: - Navigation
+    /// переход и передача данных (редактируемой транзакции) на TransactionViewController
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let transactionVC = segue.destination as? TransactionViewController else { return }
         
         if segue.identifier == "editTransaction" {
             transactionVC.editTransaction = sender as? Transact
         }
-        transactionVC.delegate = self // обязательно для передачи транзакции !!!
+        transactionVC.delegate = self /// обязательно для передачи транзакции !!!
     }
-    
+    /// Для возврата к данному VC
     @IBAction func unwind(segue: UIStoryboardSegue) {    }
 }
 
 // MARK: - Table View Data Source & Delegate
 extension AccountViewController: UITableViewDelegate, UITableViewDataSource {
+    /// Указываем количество строк в секции tableView в зависимости отфильтрованы транзакции или нет
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         if isFiltering { return filteredTransactions.count }
         return transactions.isEmpty ? 0 : transactions.count
     }
-    
+    /// Заполняем строки tableView в зависимости отфильтрованы транзакции или нет
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "transactionCell", for: indexPath) as! CustomTableViewCell
-        
         let transaction = transactionToDisplayAt(indexPath: indexPath)
         return CustomTableViewCell.createCustomCell(cell, transaction)
     }
-    
+    /// Добавляем кнопки действий по свайпу справа на лево
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
+        /// Вытаскиваем выбраную транзакцию
         let transaction = transactionToDisplayAt(indexPath: indexPath)
-        
+        /// При редактировании делаем переход на TransactionVC и передаем выбраную транзакцию
         let swipeEdit = UIContextualAction(style: .normal, title: "Редактировать") { (action, editView, success) in
             self.editTransIndexPath = indexPath
             self.performSegue(withIdentifier: "editTransaction", sender: transaction)
         }
         swipeEdit.image = UIImage(systemName: "square.and.pencil")
-        
+        /// При выборе "Удалить" удаляем из массива TableView и из памяти StorageManager и перезагружаем баланс кошелька
         let  swipeDelete = UIContextualAction(style: .destructive, title: "Удалить") { (action, delView, success) in
             self.transactions.remove(at: indexPath.row)
             StorageManager.shared.deleteTransaction(transaction)
@@ -143,55 +142,55 @@ extension AccountViewController: UITableViewDelegate, UITableViewDataSource {
         }
         swipeDelete.image = UIImage(systemName: "trash")
         
-        return UISwipeActionsConfiguration(actions: [swipeDelete, swipeEdit])  // "Редак-ть" и "Удалить" по свайпу
+        return UISwipeActionsConfiguration(actions: [swipeDelete, swipeEdit])  /// "Редак-ть" и "Удалить" по свайпу
     }
-    
+    ///Устанавливаем высоту ячейки tableView
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { 60 }
 }
 
 
 // MARK: - NewTransactionViewControllerDelegate
 extension AccountViewController: NewTransactionViewControllerDelegate {
+    /// Функция вызывается из TransactionVC через делегата для внесения новой транзакции
     func saveTransaction(_ newTransaction: Transact) {
-        
-        if let selectedIndexPath = editTransIndexPath?.row {          // -------------редактирование существующей транзакции
-            
-            transactionTableView.deselectRow(at:editTransIndexPath!, animated: true) // убираем выделение с ред. ячейки
+        /// Проверяем редактировали ли мы транзакцию или пришла новая
+        if let selectedIndexPath = editTransIndexPath?.row {          /// редактирование существующей транзакции
+            transactionTableView.deselectRow(at:editTransIndexPath!, animated: true) /// убираем выделение с ред. ячейки
             let edittingTransaction = transactionToDisplayAt(indexPath: editTransIndexPath!)
+            StorageManager.shared.deleteTransaction(edittingTransaction) /// удаляем старую версию транзакции
             
-            StorageManager.shared.deleteTransaction(edittingTransaction) // удаляем старую версию транзакции
-            
-            if isFiltering {                             // если редактировали транз-ю в отфильтрованном списке
-                filteredTransactions[selectedIndexPath] = newTransaction
+            if isFiltering {                             /// если редактировали транз-ю в отфильтрованном списке
+                filteredTransactions[selectedIndexPath] = newTransaction   /// находим старую версию транзакции в отфильтрованом списке
                 
                 for (index,transaction) in transactions.enumerated() {
                     if transaction == edittingTransaction {
                         transactions[index] = newTransaction
                     }
-                    self.editTransIndexPath = nil
+                    self.editTransIndexPath = nil   ///обнуляем IndexPath редактируемой строки
                 }
             } else {
                 transactions[selectedIndexPath] = newTransaction
             }
             
-            
-        } else {        //  --------------Если добавляется новая транзакция
-            
-            self.transactions.append(newTransaction)                // передача и добавление новой трансакции в массив транзакций
-            self.transactionTableView.insertRows(           // отображение на экране
+        } else {
+            /// Если нас пришла НОВАЯ (а не редактируемая) транзакция
+            self.transactions.append(newTransaction)                /// передача и добавление новой трансакции в массив транзакций
+            self.transactionTableView.insertRows(           /// отображение на экране в TableView
                 at: [IndexPath(row: self.transactions.count - 1, section: 0)],
                 with: .automatic
             )
         }
-        reloadTransactArrayToFiltered()
-        reloadDataTableView()
-        reloadWalletBalance()
+        
+        sortedByDateAllTransactArrays() ///сортировка основного массива `transactions` и отфильтрованого `filteredTransactions` по дате
+        reloadDataTableView()           /// Перезагрузка/обновление транзакций отображающихся в TableView параллельно в основном потоке
+        reloadWalletBalance()           /// Расчет и вывод баланса кошелька по текущим транзакциям
     }
 }
 
 
 // MARK: - SearchBarDelegate
 extension AccountViewController: UISearchBarDelegate {
+    /// Настройка SearchBar, установка критериев фильтрования списка транзакций
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
--- FinBook/Controllers/TransactionViewController.swift
+++ FinBook/Controllers/TransactionViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-// MARK: VC для ввода новой или редактирования транзакции
+// MARK: VC для создания новой или редактирования существующей транзакции
 class TransactionViewController: UIViewController {
 
     // MARK: - IBOutlets
@@ -56,7 +56,7 @@ class TransactionViewController: UIViewController {
     
 // MARK: - IBActions
     
-    /// При переключении элемента SegmentedControl доход/расход изменяет соответствующие категории и некоторые наименования
+    /// При переключении элемента `SegmentedControl` доход/расход изменяет соответствующие категории и некоторые наименования
     @IBAction func incomeStatusOnSegmentedControl(_ sender: Any) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
@@ -70,12 +70,12 @@ class TransactionViewController: UIViewController {
         setupCategoryPickerView()
     }
     
-    /// Выход из VC при нажатии кнопки "Готово"
+    /// Выход из VC при нажатии кнопки `Готово`
     @IBAction func doneButtonAction(_ sender: Any) {
         saveAndExit()
     }
     
-    /// Выход из VC при нажатии кнопки "Отмена"
+    /// Выход из VC при нажатии кнопки `Отмена`
     @IBAction func cancelButtonAction(_ sender: Any) {
         dismiss(animated: true)
     }
@@ -95,25 +95,24 @@ class TransactionViewController: UIViewController {
         edittingTransactionMode()
     }
     
-    /// Функция собирающая новую/редактируемую транзакцию из данных с полей
-    /// сохраняющая ее в памяти и отсылающая ее в AccountVC через делегат
+    /// Собираем, сохраняем и передаем транзакцию в AccountVC
     private func saveAndExit() {
         guard let costPrice = Double(costTextField.text ?? "0.00") else { return }
         guard let description = descriptionTextField.text else { return }
         
-        /// Собирает и сразу сохраняет в Storage Manager новую транзакцию
+        /// Собираем транзакцию и сразу сохраняем в Storage Manager
         let transaction = StorageManager.shared.createTransact(cost: costPrice,
                                                                description: description,
                                                                category: selectedModel.title,
                                                                date: datePickerView.date,
                                                                note: noteTextField.text ?? "",
                                                                income: isIncome)
-        /// Передает транзакцию в AccountVC через делегат
+        /// Передаем транзакцию в AccountVC через делегат
         delegate.saveTransaction(transaction)
         dismiss(animated: true)
     }
     
-    /// Функция определяет редактируем ли мы транзакцию и заполняет соответствующие поля ее свойствами
+    /// Заполняем поля при редактировании транзакции
     private func edittingTransactionMode() {
         guard let editTransaction = editTransaction else { return }
         if editTransaction.incomeTransaction == true {
@@ -130,7 +129,7 @@ class TransactionViewController: UIViewController {
         doneButton.isEnabled = true
     }
     
-    /// Функция ищет категорию редактируемой тразакции и выставляет ее
+    /// Ищем категорию редактируемой тразакции и выставляем ее в поле категории
     private func setCategoryEditTransaction() {
         for (index, value) in categoryPickerModels.enumerated() {
             if value.title == editTransaction?.category {
@@ -141,6 +140,7 @@ class TransactionViewController: UIViewController {
         setupCategoryTextField()
     }
     
+    /// Стартовая настройка поля стоимости
     private func setupCostTextField() {
         costTextField.becomeFirstResponder()
         costTextField.smartInsertDeleteType = UITextSmartInsertDeleteType.no
@@ -158,12 +158,14 @@ class TransactionViewController: UIViewController {
         categoryTextField.inputView = categoryPickerView
     }
     
+    /// Установка изображения и названия ВЫБРАНОЙ категории в соответствующие поля
     private func setupCategoryTextField() {
         categoryTextField.text = selectedModel.title
         categoryImage.image = selectedModel.icon
     }
         
-    private func setupDateTextField() { // настройка текстового поля с датой
+    /// Стартовая настройка поля даты
+    private func setupDateTextField() {
         dateTextField.text = DateConvertService.convertDateToStr(datePickerView.date)
         
         dateTextField.inputView = datePickerView
@@ -174,6 +176,7 @@ class TransactionViewController: UIViewController {
         datePickerView.addTarget(self, action: #selector(dateTextFieldDidChanged), for: .valueChanged)
     }
     
+    ///  Настройка поля над клавиатурой ( Toolbar ) как акссесуара к текстовым полям
     private func setupDoneToolBar() {
         let doneToolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 50))
         let hideKeyboardButton  = UIBarButtonItem(title: "Скрыть", style: .plain,
@@ -196,21 +199,22 @@ class TransactionViewController: UIViewController {
 
 // MARK: - PickerControl Settings
 extension TransactionViewController: UIPickerViewDataSource, UIPickerViewDelegate  {
+    /// Количество компонентов PickerView
     func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
-
+    /// Количество строк (ячеек) в компоненте PickerView
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return categoryPickerModels.count
     }
-    
+    /// Высота ячейки PickerView с категорией
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat { 20.0 }
-    
+    /// Ширина ячейки PickerView с категорией
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat { 130 }
-    
+    /// Заполнение ячеек PickerView
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         let model = categoryPickerModels[row]
         return CategoriesView.create(icon: model.icon, title: model.title)
     }
-    
+    /// Заполнение заполнение текстового поля `costTextField` и  установка картинки в `categoryImage` выбраной категории в `PickerView`
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         selectedModel = categoryPickerModels[row]
         categoryTextField.text = selectedModel.title
@@ -220,23 +224,23 @@ extension TransactionViewController: UIPickerViewDataSource, UIPickerViewDelegat
 
 // MARK: - CostTextField Settings
 extension TransactionViewController: UITextFieldDelegate {
-     
+    /// Скрыть клавиатуру при нажатии по пустому месту
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
-    
+    /// При изменении значения `costTextField` активировать или кнопку `Готово`
     @objc private func costTextFieldDidChanged() {
         guard let costName = costTextField.text else { return }
         doneButton.isEnabled = !costName.isEmpty ? true : false
     }
-    
+    /// Отображаем `Date` в `dateTextField` как `String`
     @objc private func dateTextFieldDidChanged() {
         dateTextField.text = DateConvertService.convertDateToStr(datePickerView.date)
     }
-    
+    /// Прячем клавиатуру по нажатии кнопки `Скрыть` на `Toolbar`
     @objc private func hideKeyboard() { view.endEditing(true) }
-    
+    /// Контролируем входные данные в  КАЖДЫЙ `textField` в текущем времени - количество  символов, наличие одной точки или запятой
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let textFieldText = textField.text,
               let rangeOfTextToReplace = Range(range, in: textFieldText) else {
@@ -248,7 +252,7 @@ extension TransactionViewController: UITextFieldDelegate {
         let count = textFieldText.count - substringToReplace.count + string.count
         return count <= 30
     }
-    
+    ///Переходим на следующее поле при нажатии кнопки `return button` на клавиатуре или скрываем её если последнее поле
    @objc func textFieldShouldReturn() -> Bool {
         if costTextField.isEditing  {
             guard costFormatter(cost: costTextField.text) > 0 else {
@@ -263,7 +267,7 @@ extension TransactionViewController: UITextFieldDelegate {
         } else { view.endEditing(true) }
         return true
     }
-        
+    /// Переводим значение  из `String` в `Double` (исп-ся в `costTextField`) и формирует точку или запятую в зависимости от региона
     private func costFormatter(cost: String?) -> Double {
         let formatter = NumberFormatter()
         var doubCost: Double = 0.00000
@@ -278,6 +282,7 @@ extension TransactionViewController: UITextFieldDelegate {
 
 // MARK: - Alert Controller
 extension TransactionViewController {
+    /// Выводим сообщение об ошибке в `Alert Controller`
     private func showAlert(title: String, message: String, textField: UITextField? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { _ in
